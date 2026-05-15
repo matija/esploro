53ca49b84d1acddf4be8586c1c69deee16f6a4e6
Matija Munjaković <mmunjako@gmail.com>
2026-05-15 13:26:24 +0200

    refactor: drop leading v from tag pattern and release name

--- homebrew/Casks/esploro.rb
+++ homebrew/Casks/esploro.rb
@@ -2,9 +2,9 @@ cask "esploro" do
   version "0.4.6"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
 
-  url "https://github.com/matija/esploro/releases/download/v#{version}/Esploro_#{version}_aarch64.dmg"
+  url "https://github.com/matija/esploro/releases/download/#{version}/Esploro_#{version}_aarch64.dmg"
   name "Esploro"
-  desc "A fast PostgreSQL and MySQL client"
+  desc "Modern. Sleek. Fast. A SQL client for Mac that does PostgreSQL and MySQL without the cruft."
   homepage "https://github.com/matija/esploro"
 
   app "Esploro.app"
